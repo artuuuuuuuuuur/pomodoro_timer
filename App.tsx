c0b0464f058b7f260631491796fbaa4fb2d4eeb0
artur <arturdemaria7@gmail.com>
2023-03-23 23:03:39 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -16,5 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
 });
