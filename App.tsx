2192240ce482a640804afb31d75a908250477a31
artur <arturdemaria7@gmail.com>
2023-03-24 00:09:26 -0300

    create timer components and play and pause button

--- App.tsx
+++ App.tsx
@@ -1,14 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { TimerCountDownDisplay } from './components/TimerCountDownDisplay';
+import { TimerToggleButton } from './components/TimerToggleButton';
+
+const FOCUS_TIME_MINUTES = 0.2 * 60 * 1000;
+const BREAK_TIME_MINUTES = 0.1 * 60 * 1000;
+
 
 export default function App() {
 
-  
+  const [timerCount, setTimerCount] = useState<number>(FOCUS_TIME_MINUTES);
+  const [timerInterval, setTimerInterval] = useState<NodeJS.Timer | null>(null);
+  const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
+
+  const startTimer = () => {
+    setIsTimerRunning(true);
+    const id = setInterval(() => setTimerCount(prev => prev - 1000), 1000)
+    setTimerInterval(id);
+  };
+
+  const stopTimer = () => {
+    if (timerInterval != null) {
+      clearInterval(timerInterval)
+    }
+    setIsTimerRunning(false);
+  }
 
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TimerToggleButton isTimerRunning={isTimerRunning} startTimer={startTimer} stopTimer={stopTimer} />
+      <TimerCountDownDisplay timerDate={new Date(timerCount)} />
     </View>
   );
 }
@@ -19,6 +43,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    
+
   },
 });
